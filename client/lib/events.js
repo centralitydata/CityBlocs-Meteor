d45cd5d7bf655f952b5d81a7fb713ce3a052fc17
Michael Underwood <mike.underwood@gmail.com>
2015-03-29 17:40:47 -0600

    Replace custom 'which page' logic with Iron Router

--- client/lib/events.js
+++
@@ -1,9 +0,0 @@
-Template.body.events({
-	'click .nav-home': function () {
-		Session.set('currentPage', 'home');
-	},
-
-	'click .nav-about': function () {
-		Session.set('currentPage', 'about');
-	}
-})
--- client/lib/helpers.js
+++
@@ -1,5 +0,0 @@
-Template.body.helpers({
-  pageIs: function (targetPage) {
-		return targetPage === Session.get('currentPage');
-	}	
-});
