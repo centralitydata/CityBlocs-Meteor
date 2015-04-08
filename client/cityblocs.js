e0836ed92346d90d92a70564ddb79c549eed8561
Michael Underwood <mike.underwood@gmail.com>
2015-04-07 23:16:39 -0600

    Add accounts-password, accounts-ui, and basic login functionality

--- client/cityblocs.js
+++ client/cityblocs.js
@@ -1,6 +1,14 @@
 Meteor.subscribe('cities');
 Meteor.subscribe('about_text');
 
+Accounts.config({
+	sendVerificationEmail: true,
+	restrictCreationByEmailDomain: 'centrality.ca'
+});
+Accounts.ui.config({
+	passwordSignupFields: 'USERNAME_AND_EMAIL'
+});
+
 Template.cityList.helpers({
 	cities: function () {
 		return Cities.find({}, {sort: {name: 1}});
