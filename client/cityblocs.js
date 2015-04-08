1c94f0b9dd20e4c5b1c52d34d1f25ee04650c75b
Michael Underwood <mike.underwood@gmail.com>
2015-04-08 00:46:49 -0600

    Add template helper to style active navbar elements. Helper is working by CSS is not.

--- client/cityblocs.js
+++ client/cityblocs.js
@@ -1,10 +1,6 @@
 Meteor.subscribe('cities');
 Meteor.subscribe('about_text');
 
-Accounts.config({
-	sendVerificationEmail: true,
-	restrictCreationByEmailDomain: 'centrality.ca'
-});
 Accounts.ui.config({
 	passwordSignupFields: 'USERNAME_AND_EMAIL'
 });
@@ -13,4 +9,15 @@ Template.cityList.helpers({
 	cities: function () {
 		return Cities.find({}, {sort: {name: 1}});
 	}
+});
+
+Template.registerHelper('activeRoute', function (name, class_name) {
+	var ret = '';
+
+	if (Router.current()) {
+		var loc = Router.current().location.get().path;
+		if (loc === name) { ret = class_name; }
+	}
+
+	return ret;
 });
